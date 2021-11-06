ec6311c545148d5686fff88f013c6e2523702ec3
Neha <nehaprajapati1150@gmail.com>
2021-11-06 15:36:15 +0530

    Axios-github-users

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Search = ({ searchKeyWord,setSearchKeyWord, fetchAllRes}) => {
     return (
       <>
-     <h2 className="head">Find GITHUB Users Here</h2>
+     <h3 className="head">Find GITHUB Users Here</h3>
         <form class="form-inline d-flex w-50 py-4">
         <input value={searchKeyWord} onChange={(e) => setSearchKeyWord(e.target.value)} class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
         <button class="btn btn-outline-success mx-2" onClick={fetchAllRes} type="button">Search</button>
